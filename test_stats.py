28cd8fc836daf2ff3dbc08fdafd15e68db88f417
Marchel Hill <marchel.hill@wisc.edu>
2016-01-15 13:59:28 -0600

    assert equal added

--- test_stats.py
+++ test_stats.py
@@ -1,14 +1,15 @@
 from stats import mean
+from nose.tools import assert_equal
 
 def test_mean():
-    assert mean([2,4]) == 3.0
-test_mean()
+    assert_equal(mean([2,4]) == 3.0)
+#test_mean()
 
 def test_empty_list():
-    assert mean ([]) == 0.0
-test_empty_list()
+    assert_equal(mean ([]) == 0.0)
+#test_empty_list()
 
 def test_float_mean():
     assert mean([.5,.5,1]) == .6666
-test_float_mean()_
+#test_float_mean()_
 
