79cd81d4351efd419fb5b39999ecbab3931a428c
Marchel Hill <marchel.hill@wisc.edu>
2016-01-15 13:42:19 -0600

    add function to if

--- stats.py
+++ stats.py
@@ -3,11 +3,17 @@ def mean(vals):
     assert type(vals) is list, 'wrong input format'
     total = sum(vals)
     length = len(vals)
-    return total/length
+    if length == 0:
+       return 0.0
+    else:
+       return total/length
 
 def test_mean():
-   assert mean([2,4]) == 3.0
-
+    assert mean([2,4]) == 3.0
 test_mean()
+
+def test_empty_list():
+    assert mean ([]) == 0.0
+test_empty_list()
 
 #print(mean("hello"))
