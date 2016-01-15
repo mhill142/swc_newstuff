3048fc0e2988b405bf3d87db9e71776747edee6b
Marchel Hill <marchel.hill@wisc.edu>
2016-01-15 13:22:23 -0600

    adding assert to list

--- stats.py
+++ stats.py
@@ -1,5 +1,6 @@
 def mean(vals):
     """Calculate the arithmetic mean of a list of numbers in vals"""
+assert type(vals) is list, 'wrong input format'
     total = sum(vals)
     length = len(vals)
     return total/length
