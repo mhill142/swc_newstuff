a5ec586bdce92ef37bdc2aabf81897796504b602
Marchel Hill <marchel.hill@wisc.edu>
2016-01-15 13:29:06 -0600

    adding real function

--- stats.py
+++ stats.py
@@ -1,10 +1,13 @@
 def mean(vals):
     """Calculate the arithmetic mean of a list of numbers in vals"""
-assert type(vals) is list, 'wrong input format'
+    assert type(vals) is list, 'wrong input format'
     total = sum(vals)
     length = len(vals)
     return total/length
 
-#print(mean([2,4]))
+def test_mean():
+   assert mean([2,4]) == 3.0
 
-print(mean("hello"))
+test_mean()
+
+#print(mean("hello"))
