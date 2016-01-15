53a40df6a7f30f1451cb46465f785868fcaac5f6
Marchel Hill <marchel.hill@wisc.edu>
2016-01-15 13:20:12 -0600

    breaking code

--- stats.py
+++ stats.py
@@ -1,8 +1,9 @@
-  def mean(vals):
+def mean(vals):
     """Calculate the arithmetic mean of a list of numbers in vals"""
     total = sum(vals)
     length = len(vals)
     return total/length
 
-print(mean([2,4]))
+#print(mean([2,4]))
 
+print(mean("hello"))
