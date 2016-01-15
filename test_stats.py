a3dd35c12f8fbfbb163fc37dda240f3ef9704ca4
Marchel Hill <marchel.hill@wisc.edu>
2016-01-15 13:49:03 -0600

    moving test

---
+++ test_stats.py
@@ -0,0 +1,14 @@
+from stats import mean
+
+def test_mean():
+    assert mean([2,4]) == 3.0
+test_mean()
+
+def test_empty_list():
+    assert mean ([]) == 0.0
+test_empty_list()
+
+def test_float_mean():
+    assert mean([.5,.5,1]) == .6666
+test_float_mean()_
+
